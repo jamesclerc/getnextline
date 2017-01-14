1d433a18db96148f1e981d47804bcf34eb6b078f
james.clerc <james.clerc@epitech.eu>
2017-01-14 13:36:47 +0100

    adding the true get next line

--- get_next_line.c
+++ get_next_line.c
@@ -5,34 +5,34 @@
 ** Login   <james.clerc@epitech.net>
 ** 
 ** Started on  Thu Jan 12 15:40:43 2017 james clerc
-** Last update Thu Jan 12 23:56:58 2017 james clerc
+** Last update Sat Jan 14 13:36:11 2017 james clerc
 */
 
 #include "get_next_line.h"
 
-void	condition_read(int fd, int *readd, int *i, char *save)
+void	condition_read(int fd, int *readd, int *taille, char *save)
 {
   if (*readd <= *i)
     {
       *readd = read(fd, save, READ_SIZE);
-      *i = 0;
+      *taille = 0;
     }
 }
 
-void	add_charact(char *ret, char *save, int *i, int *comp)
+void	add_charact(char *ret, char *save, int *taille, int *comp)
 {
   ret[*comp] = save[*i];
   *comp = *comp + 1;
-  *i = *i + 1;
+  *taille = *taille + 1;
 }
 
-char	end_prog(char *ret, int readd, int *i, int comp)
+char	end_prog(char *ret, int readd, int *taille, int comp)
 {
   if (comp == 0 && readd == 0)
     return (0);
   else
     {
-      *i = *i + 1;
+      *taille = *taille + 1;
       ret[comp] = '\0';
       return (1);
     }
@@ -41,7 +41,7 @@ char	end_prog(char *ret, int readd, int *i, int comp)
 char	*get_next_line(const int fd)
 {
   static char	save[READ_SIZE + 2];
-  static int	i = 1;
+  static int	taille = 1;
   static int	readd = 1;
   static int	comp = 0;
   char	*ret;
@@ -52,18 +52,15 @@ char	*get_next_line(const int fd)
   size_of_malloc = 100000;
   comp = 0;
   ret = malloc(sizeof(char) * size_of_malloc);
-  while (readd != 0  && save[i] != '\n')
+  while (readd != 0  && save[taille] != '\n')
     {
-      condition_read(fd, &readd, &i, save);
-      while (readd > i && save[i] != '\n')
-	add_charact(ret, save, &i, &comp);
+      condition_read(fd, &readd, &taille, save);
+      while (readd > taille && save[taille] != '\n')
+	add_charact(ret, save, &taille, &comp);
     }
-  if (end_prog(ret, readd, &i, comp) == 0)
+  if (end_prog(ret, readd, &taille, comp) == 0)
     return (NULL);
   else
-    {
-      my_putstr(ret);
-      return (ret);
-    }
+    return (ret);
 }
 
